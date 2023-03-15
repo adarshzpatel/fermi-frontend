257adca60285df553f9ddd16a5c0e772ae6299c1
Hari Seldon <doraxfire@proton.me>
2023-03-15 09:50:24 +0530

    new constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,14 +12,14 @@
 //export const eventQPda = "5UvNSRJxWNSwVha2JK9MnXLvmoJBE7fvres48V2vAS6Y";
 
 export const PROGRAM_ADDRESS = "TtN7ndtaUUBWvkXzt5P8cSngmqLcbcMyqYyMbMsWxGN";
-export const openOrdersPda = "E11wPHRDb7vxJogMeNZj7R1rk3vHXK7y5vh24aM2ahnZ";
-export const marketPda = "8AG21rP2jrCcV6ytWSoifq8rgew7x612hs1zo1VpWRwb";
-export const coinVault = "5deg3FKh3gHMZ8fL2tFcH6j5qbrVer3XH3kYf41vfKbm";
-export const pcVault = "HpS9dTbXwWrSdfKpPFGWNjdnq7uUyLhps53oLYsLKFVk";
-export const coinMint = "Fv39ECjnvDyTBBWWACdXa9ewxVVqJ14u1mSyGfdbXYPA";
-export const pcMint = "4dRAfF5QGawnYTsyPiuAmj1ViKeiUCVXvpr3eRcMMBnT";
-export const payer = "ASe1Y6EYVGvRpWUibJA9iNba6VC7hMnCprNB6baqGJYj";
-export const bidsPda = "7QRWnzHGAVxX7nneGoTL8sz8i944DfUKxuuBvf9fxS2a";
-export const asksPda = "4CPM2qmbCjoBqA6rqafieRaPkGmPpGxBeTJcyf4a2KyD";
-export const reqQPda = "HfHgYU7jPhr3fhTDDp6XHrof2edjXo8hdKpVX3bco2fy";
-export const eventQPda = "5UvNSRJxWNSwVha2JK9MnXLvmoJBE7fvres48V2vAS6Y";
+export const openOrdersPda = "4oa82n9JznmvtgJeckt25SoLbUmzgeoopL6VeBauKiZT";
+export const marketPda = "E3Rkw7Bc6zn7eYmKWg1hapZHuYTeTRb4TtXdraehQYqG";
+export const coinVault = "DyfFTcTiAy71ALLwc1ECjW6RVypGFcZpUmLBEgQujPif";
+export const pcVault = "448maz6mSJBT85k4QXaegmftn2jH8KMAQLScXzFe2Wa3";
+export const coinMint = "6YaYCreMTbWatipC8MpJxcDgzuUzABzTixQsuHg4dBZn";
+export const pcMint = "6iK3r6CdNwxaoDPHAb42QKSRDHWiFr3ZeG5bGQbkSStq";
+export const payer = "C78SARpRzQqasHzQKUeR28rw6e6L2Hr3h3bMP8A6G5MM";
+export const bidsPda = "GatKy4WuJYE78xmUkmLMDjcs8pQYFmMuWXqPttbiRKjH";
+export const asksPda = "5f2zSCK5oZBFo5vLqrku8YcY1LUqRq3xLWEVgPpcRQdA";
+export const reqQPda = "8dTF7GkUiL43xnygEhxHGz7GVwJK4uqWWgJkW9aNiLqA";
+export const eventQPda = "FxLeq2vnac6XnCgXJiWcrUa7TxmFeqk4qRfC6GrYfpGk";
